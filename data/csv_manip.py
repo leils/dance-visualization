b35bf5c059d0069dfc03d28bd86f942a5e10e608
Leia <e.leiachang@gmail.com>
2017-01-15 17:21:29 -0800

    Pulling Right Knee data

--- data/csv_manip.py
+++ data/csv_manip.py
@@ -1,9 +1,15 @@
 import csv
+import sys
 
-with open('cut_data.csv', 'rb') as csvfile:
-  r= csv.reader(csvfile, delimiter=",", quotechar="|")
-  for row in r:
-    x = float(row[0]) / 10
-    y = float(row[1]) / 10
-    z = float(row[2]) / 10
-    print str(x) + "," + str(y) + "," + str(z) + ","
+def main(argv):
+   with open(argv[0], 'rb') as csvfile:
+      r= csv.reader(csvfile, delimiter=",", quotechar="|")
+      for row in r:
+         x = float(row[0]) / 100
+         y = float(row[1]) / 100
+         z = float(row[2]) / 100 
+         print str(x) + "," + str(y) + "," + str(z) + ","
+      return 
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- data/csv_parser.py
+++ data/csv_parser.py
@@ -1,6 +1,17 @@
+import sys
 import csv
 
-with open('data.csv', 'rb') as csvfile:
-  r= csv.reader(csvfile, delimiter=",", quotechar="|")
-  for row in r:
-    print row[1] + "," + row[2] + "," + row[3]
+
+def main(argv):
+   data_num = int(argv[0])
+   base = (data_num - 1) * 3 #The data is indexed at 1, not 0 
+
+   with open('data.csv', 'rb') as csvfile:
+     r= csv.reader(csvfile, delimiter=",", quotechar="|")
+     for row in r:
+       print row[base + 1] + "," + row[base + 2] + "," + row[base + 3]
+
+   return
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
