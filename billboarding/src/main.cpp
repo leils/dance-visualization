09850bf2d75fc75d3db5324149366deaa5f11702
Leia <e.leiachang@gmail.com>
2017-04-11 11:36:46 -0700

    Rendering shading via normals

--- billboarding/src/main.cpp
+++ billboarding/src/main.cpp
@@ -1136,7 +1136,7 @@ static void init()
     prog->addAttribute("vertPos");
     prog->addAttribute("vertNor");
     prog->addUniform("knee");
-    // prog->addUniform("lightDir");
+    prog->addUniform("lightDir");
 
     cam->init(window);
 }
@@ -1166,7 +1166,7 @@ static void render()
     prog->bind();
     glUniformMatrix4fv(prog->getUniform("P"), 1, GL_FALSE, P->topMatrix().data());
     glUniform1i(prog->getUniform("knee"), false);
-    // glUniform3f(prog->getUniform("lightDir"), -1, 0, 0);
+    glUniform3f(prog->getUniform("lightDir"), -1, 0, 0);
 
 
     // cam->mouseTracking(window, TIMESTEP);
